c8b458a86a996ced6ded2dd640e5ca369d82b5b1
Rajib Debnath <rajibdebnath923@gmail.com>
2024-11-21 12:37:38 +0530

    add

--- src/ApiFetch.js
+++ src/ApiFetch.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiKey = 'b6131b9df9mshcd77884b6a03fe6p14d712jsnadea9ca1c6d3'; // Replace with your actual RapidAPI key
+const apiKey = '7a7247164fmsh37a12bd000f2423p17242ejsnbf802574a35d'; // Replace with your actual RapidAPI key
 
 export const fetchData = async (url) => {
   try {
